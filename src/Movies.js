5c449c4044725c9bf5b6be4ffcdf8db4cd63ffeb
stepperweb <wodnqjel@gmail.com>
2021-06-05 23:49:09 +0900

    add the movie component

---
+++ src/Movies.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movies({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li className="genres_genre" key={index}>
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie_summary">{summary.slice(0, 180)}...</p>
+      </div>
+    </div>
+  );
+}
+
+Movies.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movies;
